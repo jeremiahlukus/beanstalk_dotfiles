27fcdb8e9faf46ebcad68500edf7c577508de48d
Jeremiah-Parrack <jeremiah.parrack@coxautoinc.com>
2021-12-13 11:12:16 -0500

    Jeremiah-Parrack | pass env vars

--- example_deploy.sh
+++ example_deploy.sh
@@ -13,4 +13,5 @@ export AWS_SECRET_ACCESS_KEY=your_aws_secret_key;
 bundle install
 rails db:migrate
 git ls-files | zip app.zip -@
-eb_deploy -p app.zip -e staging
+
+RAILS_MASTER_KEY="545b...530" ENVIRONMENT=staging RAILS_ENV=staging DATABASE_HOST=yourdbhost.us-east-1.rds.amazonaws.com DATABASE=jumpstart DATABASE_USERNAME=jumpstart DATABASE_PASSWORD=jumpstarttest eb_deploy -p app.zip -e staging
