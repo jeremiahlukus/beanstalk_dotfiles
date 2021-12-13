2572e72c8229dd05da7236e21d59c8fd2621be21
Jeremiah-Parrack <jeremiah.parrack@coxautoinc.com>
2021-12-12 20:49:08 -0500

    Jeremiah-Parrack | add db config

---
+++ example_deploy.sh
@@ -0,0 +1,14 @@
+export RAILS_MASTER_KEY="545b...530";
+export ENVIRONMENT=staging;
+export RAILS_ENV=staging;
+export DATABASE_HOST=yourdbhost.us-east-1.rds.amazonaws.com;
+export DATABASE=jumpstart;
+export DATABASE_USERNAME=jumpstart;
+export DATABASE_PASSWORD=jumpstarttest;
+export AWS_ACCESS_KEY_ID=your_aws_key;
+export AWS_SECRET_ACCESS_KEY=your_aws_secret_key;
+
+bundle install
+rails db:migrate
+git ls-files | zip app.zip -@
+eb_deploy -p app.zip -e staging
